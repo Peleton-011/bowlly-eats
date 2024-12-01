752841e78e9f383ee760cbde73c16b19af98b35e
Nico Diaz <nicodiazan@gmail.com>
2024-12-01 18:22:44 +0100

    Add some tailwind settings

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,19 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
-  theme: {
-    extend: {},
-  },
-  plugins: [],
-}
-
+	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}"],
+	theme: {
+		extend: {
+			"metallic-blue": "#305979",
+			"chinese-black": "#121212",
+			"emerald": "61D095",
+		},
+		fontFamily: {
+			Montserrat: "Montserrat, sans-serif",
+		},
+		container: {
+			center: true,
+			padding: "2rem",
+		},
+	},
+	plugins: [],
+};
