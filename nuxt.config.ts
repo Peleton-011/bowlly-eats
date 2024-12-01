dfd6098813fa1b2dcd22a395e3778bdd25e4a76e
Nico Diaz <nicodiazan@gmail.com>
2024-12-01 18:13:29 +0100

    Add some basic settings for some of the modules

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,33 +1,44 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
- compatibilityDate: "2024-11-01",
+	compatibilityDate: "2024-11-01",
 
- future: {
-																																																																																																																																																																																																																																																																																																																																compatibilityVersion: 4,
-																																																																},
+	future: {
+		compatibilityVersion: 4,
+	},
 
- devtools: { enabled: true },
+	devtools: { enabled: true },
 
- experimental: {
-																																																																																																																																																																																																																																																																																																																																scanPageMeta: "after-resolve",
-																																																																																																																																																																																																																																																																																																																																sharedPrerenderData: false,
-																																																																																																																																																																																																																																																																																																																																compileTemplate: true,
-																																																																																																																																																																																																																																																																																																																																resetAsyncDataToUndefined: true,
-																																																																																																																																																																																																																																																																																																																																templateUtils: true,
-																																																																																																																																																																																																																																																																																																																																relativeWatchPaths: true,
-																																																																																																																																																																																																																																																																																																																																normalizeComponentNames: false,
-																																																																																																																																																																																																																																																																																																																																defaults: {
-																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																useAsyncData: {
-																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																deep: true,
-																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																},
-																																																																																																																																																																																																																																																																																																																																},
-																																																																},
+	experimental: {
+		scanPageMeta: "after-resolve",
+		sharedPrerenderData: false,
+		compileTemplate: true,
+		resetAsyncDataToUndefined: true,
+		templateUtils: true,
+		relativeWatchPaths: true,
+		normalizeComponentNames: false,
+		defaults: {
+			useAsyncData: {
+				deep: true,
+			},
+		},
+	},
 
- unhead: {
-																																																																																																																																																																																																																																																																																																																																renderSSRHeadOptions: {
-																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																																omitLineBreaks: false,
-																																																																																																																																																																																																																																																																																																																																},
-																																																																},
+	unhead: {
+		renderSSRHeadOptions: {
+			omitLineBreaks: false,
+		},
+	},
 
- modules: ["@nuxtjs/tailwindcss", "@nuxtjs/google-fonts", "@nuxt/icon", "@nuxt/image"],
+	modules: [
+		"@nuxtjs/tailwindcss",
+		"@nuxtjs/google-fonts",
+		"@nuxt/icon",
+		"@nuxt/image",
+	],
+
+    googleFonts: {
+        families: {
+            Montserrat: true,
+        },
+    },
 });
