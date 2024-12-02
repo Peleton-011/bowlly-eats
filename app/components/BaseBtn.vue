39690145b5955acf6acf85c3a73689453e09c8f7
Nico Diaz <nicodiazan@gmail.com>
2024-12-02 21:25:37 +0100

    Move the label into a slot

--- app/components/BaseBtn.vue
+++ app/components/BaseBtn.vue
@@ -10,7 +10,9 @@ defineProps<{
 		:to="{ path: to }"
 		class="px-4 py-2 self-start bg-metallic-blue rounded-md text-base lg:text-lg cursor-pointer font-bold"
 	>
-        {{ label }}
+		<slot>
+			{{ label }}
+		</slot>
 	</NuxtLink>
 </template>
 
