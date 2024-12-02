e3ccd3f086c638c0a2fa2381acb735bfab23e7d9
Nico Diaz <nicodiazan@gmail.com>
2024-12-02 17:47:37 +0100

    Update navigation to include the about and to use NuxtLik

--- app/components/BaseNavigation.vue
+++ app/components/BaseNavigation.vue
@@ -3,13 +3,16 @@
 <template>
 	<header class="py-4 border-b">
 		<nav class="flex container items-center">
-			<a href="" class="flex gap-1 items-center text-emerald">
+			<NuxtLink to="/" class="flex gap-1 items-center text-emerald">
 				<IconHat class="w-14" color="#61D095" size="100" />
 				<span class="text-3xl font-bold">Bowlly Eats</span>
-			</a>
+			</NuxtLink>
 			<ul class="flex gap-6 ml-auto text-xl font-bold capitalize">
 				<li>
-					<a href="">Home</a>
+					<NuxtLink to="/">Home</NuxtLink>
+				</li>
+				<li>
+					<NuxtLink to="/about">About</NuxtLink>
 				</li>
 			</ul>
 		</nav>
