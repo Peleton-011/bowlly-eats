6a6ea2b3155286008a58262fe4b093bdf98d928d
Nico Diaz <nicodiazan@gmail.com>
2024-12-02 20:37:32 +0100

    Add a RecipeCard component

---
+++ app/components/RecipeCard.vue
@@ -0,0 +1,52 @@
+<script lang="ts" setup>
+import { type Recipe } from "../../types/types";
+
+defineProps<{
+	recipe: Recipe;
+}>();
+</script>
+
+<template>
+	<div class="flex flex-col shadow rounded-md">
+		<NuxtImg
+			:src="recipe.image"
+			sizes="xs:100vw sm:50vw lg:400px"
+			format="webp"
+			densities="x1"
+			alt=""
+			class="rounded-t-md"
+		/>
+		<div class="flex flex-col py-6 px-4 flex-1">
+			<p class="text-xl lg:text-2xl font-semibold mb-2">
+				{{ recipe.name }}
+			</p>
+			<div
+				class="font-normal w-full bg-[#2b2e31] flex gap-8 text-lg lg:text-xl mb-4 mt-auto pl-5 py-2 rounded-md"
+			>
+				<div class="flex items-center gap-1">
+					<Icon
+						name="mdi:clock-time-eight-outline"
+						style="color: #61d095"
+					/>
+					<span>{{ recipe.cookTimeMinutes }}</span>
+				</div>
+				<div class="flex items-center gap-1">
+					<Icon name="mdi:fire" style="color: #61d095" />
+					<span>{{ recipe.caloriesPerServing }}</span>
+				</div>
+				<div class="flex items-center gap-1">
+					<Icon name="mdi:star" style="color: #61d095" />
+					<span>{{ recipe.rating }} ({{ recipe.reviewCount }})</span>
+				</div>
+			</div>
+			<NuxtLink
+				:to="`/recipes/${recipe.id}`"
+				class="px-4 py-2 self-start bg-metallic-blue rounded-md text-base lg:text-lg cursor-pointer font-bold"
+			>
+				View
+			</NuxtLink>
+		</div>
+	</div>
+</template>
+
+<style></style>
