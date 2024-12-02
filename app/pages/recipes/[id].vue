84b0d591d761cf806a1a16a3d09f8a9cf1563fc3
Nico Diaz <nicodiazan@gmail.com>
2024-12-02 20:17:24 +0100

    Add a basic single recipe page

---
+++ app/pages/recipes/[id].vue
@@ -0,0 +1,16 @@
+<script lang="ts" setup>
+import { type Recipe } from "../../../types/types";
+const { id } = useRoute().params;
+
+const { data, error } = await useFetch<Recipe>(
+	`https://dummyjson.com/recipes/${id}`
+);
+</script>
+
+<template>
+	<div class="text-3xl">
+		<h1>{{ data?.name }}</h1>
+	</div>
+</template>
+
+<style></style>
