84935709c593be93a5177ba5d2a19d88d2d22e98
Nico Diaz <nicodiazan@gmail.com>
2024-12-02 20:26:35 +0100

    Make sure error page is processed by tailwind

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,13 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-	content: ["./index.html", "./src/**/*.{vue,js,ts,jsx,tsx}", "./app/**/*.{vue,js,ts,jsx,tsx}"],
+	content: [
+		"./index.html",
+		"./src/**/*.{vue,js,ts,jsx,tsx}",
+		"./app/**/*.{vue,js,ts,jsx,tsx}",
+		"./app/*.{vue,js,ts,jsx,tsx}",
+	],
 	theme: {
 		extend: {
 			colors: {
 				"metallic-blue": "#305979",
 				"chinese-black": "#121212",
-				"emerald": "#61D095",
-                "clear": "#a2b1c2"
+				emerald: "#61D095",
+				clear: "#a2b1c2",
 			},
 		},
 		fontFamily: {
