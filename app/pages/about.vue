a7da5a329802765dbf080a8041c144e76ec5b018
Nico Diaz <nicodiazan@gmail.com>
2024-12-02 17:47:26 +0100

    Add a basic about page

---
+++ app/pages/about.vue
@@ -0,0 +1,17 @@
+<template>
+	<main>
+		<section class="container py-20">
+			<h1 class="text-6xl font-extrabold mb-6 text-balance">
+				About Bowlly Eats:
+			</h1>
+			<p class="text-xl font-extrabold mb-6 text-balance max-w-screen-lg">
+				Bowlly Eats is a recipe application using the dummyJSON API,
+				created by Nico Diaz for personal use.
+			</p>
+		</section>
+	</main>
+</template>
+
+<script lang="ts" setup></script>
+
+<style></style>
