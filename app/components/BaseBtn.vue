939badccdc8177d92ceacf1915be779c34753756
Nico Diaz <nicodiazan@gmail.com>
2024-12-02 21:23:45 +0100

    Add a BaseBtn component

---
+++ app/components/BaseBtn.vue
@@ -0,0 +1,17 @@
+<script lang="ts" setup>
+defineProps<{
+	label?: string;
+	to?: string;
+}>();
+</script>
+
+<template>
+	<NuxtLink
+		:to="{ path: to }"
+		class="px-4 py-2 self-start bg-metallic-blue rounded-md text-base lg:text-lg cursor-pointer font-bold"
+	>
+        {{ label }}
+	</NuxtLink>
+</template>
+
+<style></style>
