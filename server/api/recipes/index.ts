9a9d75276bcc63cdfb6835a26332f0f9b222f778
Nico Diaz <nicodiazan@gmail.com>
2024-12-13 12:59:36 +0100

    Fix index so it returns a single array of Recipe objects including both internal and external

--- server/api/recipes/index.ts
+++ server/api/recipes/index.ts
@@ -1,5 +1,30 @@
 import data from "@/../assets/recipes.json";
+import { type Recipe } from "../../../types/types";
+import { type RecipeResponse } from "../../../types/types";
 
-export default defineEventHandler(async (event) => {
-	return data;
+export default defineEventHandler(async () => {
+	const internalRecipes = data.map((recipe, index) => ({
+		...recipe,
+		id: index + 1, // Assign ID based on index
+	}));
+
+	// Fetch external recipes from the API
+	let externalRecipes: Recipe[] = [];
+	try {
+		const fetchedResponse = await globalThis.$fetch<RecipeResponse>(
+			"https://dummyjson.com/recipes?limit=6"
+		);
+
+		externalRecipes = fetchedResponse.recipes.map((recipe, index) => ({
+			...recipe,
+			id: internalRecipes.length + index + 1, // Assign ID sequentially after internal recipes
+		}));
+	} catch (error) {
+		console.error("Error fetching external recipes:", error);
+	}
+
+	// Combine internal and external recipes
+	const allRecipes = [...internalRecipes, ...externalRecipes];
+
+	return allRecipes;
 });
